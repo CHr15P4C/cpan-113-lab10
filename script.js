1a6ff86799bf2c422a6c53d46494ebed413a1114
CHr15P4C <cspacitt@gmail.com>
2025-12-03 17:32:28 -0500

    added base class prod prop

---
+++ script.js
@@ -0,0 +1,10 @@
+//create a base class product properties with name price and quantity
+
+class ProductProperties {
+    //constructor method
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
